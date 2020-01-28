7f9d87b32266f9a061a852cf3c86003448655d73
Lukas Weinel <lukasweinel@gmail.com>
2020-01-28 21:40:00 +0800

    Continuous fire
    added the function to fire continuously when holding down space

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,10 @@ public class Player : MonoBehaviour
     [SerializeField] float padding = 1f;
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.5f;
+
+    Coroutine firingCoroutine;
+
     float xMin;
     float xMax;
     float yMin;
@@ -32,12 +36,23 @@ public class Player : MonoBehaviour
 
     private void Fire()
     {
-        if(Input.GetButtonDown("Fire1")){
+       if(Input.GetButtonDown("Fire1")){
+           firingCoroutine = StartCoroutine(FireContinuously());   
+        }
+       if(Input.GetButtonUp("Fire1")){
+           StopCoroutine(firingCoroutine);
+       }
+    }
+
+    IEnumerator FireContinuously(){ 
+        while (true){
             GameObject laser = Instantiate(
                 laserPrefab, 
                 transform.position, 
                 Quaternion.identity) as GameObject;
             laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+
+        yield return new WaitForSeconds(projectileFiringPeriod);
         }
     }
 
